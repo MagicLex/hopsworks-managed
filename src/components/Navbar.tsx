03f0370a343fb4ed1ba58c441ebabf3f516c9116
MagicLex <lex@logicalclocks.com>
2025-07-22 15:07:33 +0300

    Make navbar logo clickable - links to dashboard for logged in users

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Box, Flex } from 'tailwind-quartz';
 import { UserProfile } from './UserProfile';
+import { useAuth } from '@/contexts/AuthContext';
 
 const Navbar: React.FC = () => {
+  const { user } = useAuth();
+  
   return (
     <Box as="nav" className="border-b border-grayShade2 bg-white">
       <Box className="max-w-7xl mx-auto px-4">
         <Flex align="center" justify="between" className="h-14">
-          <Image 
-            src="/logo_hopsworks.svg" 
-            alt="Hopsworks" 
-            width={140} 
-            height={32}
-          />
+          <Link href={user ? "/dashboard" : "/"}>
+            <Image 
+              src="/logo_hopsworks.svg" 
+              alt="Hopsworks" 
+              width={140} 
+              height={32}
+              className="cursor-pointer"
+            />
+          </Link>
           <UserProfile />
         </Flex>
       </Box>
